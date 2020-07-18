7f55e2670c4c373106c5ea7a36e6d0b55660edcb
Vi <huynhtrandinhvi@gmail.com>
2020-07-18 12:22:38 +0700

    day 5

--- src/test/java/locateElements/BasicAuthenticationTest.java
+++ src/test/java/locateElements/BasicAuthenticationTest.java
@@ -1,4 +1,13 @@
 package locateElements;
 
+import static Supports.Browser.openBrowser;
+import static Supports.Browser.visit;
+
 public class BasicAuthenticationTest {
+    public static void main(String[] args) {
+        openBrowser("chrome");
+        visit("https://the-internet.herokuapp.com/basic_auth");
+
+
+    }
 }
--- src/test/java/locateElements/DropdownTest.java
+++ src/test/java/locateElements/DropdownTest.java
@@ -1,4 +1,16 @@
 package locateElements;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
+
+import static Supports.Browser.*;
 
 public class DropdownTest {
+    public static void main(String[] args) {
+        openBrowser("chrome");
+        visit("https://the-internet.herokuapp.com/dropdown");
+
+        selectByIndex(How.ID, "dropdown", 1); // index
+        selectByValue(How.ID, "dropdown", "2"); //attribute
+        //selectByVisibleText(How.ID, "dropdown", "Option 2"); // text
+    }
 }
