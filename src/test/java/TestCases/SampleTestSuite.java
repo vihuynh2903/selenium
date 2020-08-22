c4bc86f0cbff768468b74ce115ca80d6dccce0da
Vi <huynhtrandinhvi@gmail.com>
2020-08-22 12:07:31 +0700

    TestNG prefit: beforeSuite, beforeClass,.... afterSuite, afterMethod,...

--- src/test/java/TestCases/SampleTestSuite.java
+++ src/test/java/TestCases/SampleTestSuite.java
@@ -1,8 +1,54 @@
 package TestCases;
 
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class SampleTestSuite {
+
+    @BeforeSuite
+    void beforeSuite(){
+        System.out.println("before suite");
+    }
+
+    @BeforeTest
+    void beforeTest(){
+        System.out.println("before test");
+    }
+
+    @BeforeClass
+    void beforeClass(){
+        System.out.println("before class");
+    }
+
+    @BeforeMethod
+    void beforeMethod(){
+        System.out.println("before method");
+    }
+
+    @BeforeGroups
+    void beforeGroups(){
+        System.out.println("before group");
+    }
+
+    @AfterMethod
+    void afterMethod(){
+        System.out.println("after method");
+    }
+
+    @AfterClass
+    void afterClass(){
+        System.out.println("after class");
+    }
+
+    @AfterTest
+    void afterTest(){
+        System.out.println("after test");
+    }
+
+    @AfterSuite
+    void afterSuite(){
+        System.out.println("after suite");
+    }
+
     @Test(groups = {"smoke"})
     void tc01(){
         System.out.println("the first test");
